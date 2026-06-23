57fd3c304fc6321f3e20e02637ef4be4292bb056
Vincenzo La Rosa <v.larosa.dev@gmail.com>
2026-06-23 20:04:19 +0200

    minor fix

--- components/atoms/ambientLayer.tsx
+++ components/atoms/ambientLayer.tsx
@@ -2,7 +2,6 @@ import { AmbientOrbs } from './ambientOrbs';
 
 export const AmbientLayer = () => (
   <div className="ambient-layer" aria-hidden>
-    <div className="ambient-chrome-edge ambient-chrome-edge--bottom" />
     <AmbientOrbs />
   </div>
 );
